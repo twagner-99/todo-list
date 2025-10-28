011f01b251660b23b2e5b92bcb1d52da782f2cd9
twagner-99 <wagnertaylor1993@gmail.com>
2025-10-28 14:41:59 -0700

    Add fns to display todo list.

--- src/user-interface.js
+++ src/user-interface.js
@@ -1,29 +1,65 @@
 import { getTodoList } from "./todo-items";
 
+const todoListDiv = document.querySelector('#todo-list');
+
 const displayTodoListAll = () => {
-    const todoList = getTodoList();
+    const todoList = getTodoList();     // Do this each time to get current snapshot?
 
-
-    console.log(todoList);
-    // Inital render and will run on "All Items" btn click.
-    // For each key in todoList, displayProject (use for key in loop)
-}
-
-const displayProject = (project) => {
-    const todoList = getTodoList();
-    for (let todoItem of todoList[project]) {
-
+    for (let project in todoList) {
+        displayProject(project);
     }
+}
+
+const displayProject = (project) => {   // If project is default, don't create a header?
+    const projectDiv = renderTodoList(project);
     
-    console.log(todoList[project]);
-    // On project btn click, project will get passed in so correct project is displayed.
-    // For each todoItem in a project, create a heading and divs and then rednerTodoItems fn
+    // const projectDiv = renderProjectContainer(project);
+    // const todoList = getTodoList();
 
+    // for (let todoItem of todoList[project]) {
+    //     const todoItemDiv = renderTodoItem(todoItem);
+    //     projectDiv.appendChild(todoItemDiv);
+    // }
+
+    todoListDiv.appendChild(projectDiv);
 }
 
-const renderTodoItems = () => {
-    // Use this fn to add any divs created from displayTodoList/displayProject to the UI
+const renderTodoList = (project) => {   // Is it better to have this function? Or just have it all live under displayProject?
+    const projectDiv = renderProjectContainer(project);
+    const todoList = getTodoList();
+
+    for (let todoItem of todoList[project]) {
+        const todoItemDiv = renderTodoItem(todoItem);
+        projectDiv.appendChild(todoItemDiv);
+    }
+
+    return projectDiv;
 }
+
+const renderProjectContainer = (project) => {
+    const projectDiv = document.createElement('div');
+    const projectHeader = document.createElement('h1');
+    
+    projectDiv.id = project;
+    projectHeader.textContent = project;
+
+    projectDiv.appendChild(projectHeader);
+    return projectDiv;
+}
+
+const renderTodoItem = (todoItem) => {
+    const todoItemDiv = document.createElement('div');
+    const todoItemPara = document.createElement('p');
+
+    todoItemDiv.dataset.uuid = todoItem.uuid;
+    todoItemPara.textContent = todoItem.title;
+
+    todoItemDiv.appendChild(todoItemPara);
+    return todoItemDiv;
+}
+
 // get todoList, cycle thru all items and display
 // Under all items, we need to show My Stuff, and any other projects
 // Under individual projects, only show that stuff
+
+export { displayTodoListAll, };
